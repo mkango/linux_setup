5a9da9d16c96ddad47951622d15796ea871e0177
Roshen Mathew <rmathew174@gmail.com>
2019-11-26 17:18:15 +1100

    update git_setup.sh

--- git_setup.sh
+++ git_setup.sh
@@ -2,4 +2,6 @@
 sudo apt update -y
 sudo apt install git-all -y
 
-sudo snap install gitkraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+sudo rm gitkraken-amd64.deb
